f1862520a946a0c2b2cea0801902cb87d843ca95
Rent Sidon <rsidon@icloud.com>
2016-09-11 23:41:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var cart;
+
+// Define a global variable (use `var` at the top level) called `cart`.
+//Initialize it as an empty array.
+
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -14,4 +18,38 @@ function total() {
   }
 
   return t
+}
+
+//+ Define a function `getCart` that takes no arguments and returns the `cart`.
+
+function getCart() {
+  return cart
+}
+
+//Define a function `addToCart`. This function should accept one argument, the item the user wants to purchase.
+//    This function should automatically set a price for this item by generating a random number between 0 and 100.
+var price = Math.floor(Math.random() * 100)
+function addToCart(item) {
+   cart.push({ [item]: price})
+  console.log(`${item} has been added to your cart.`);
+
+  return cart
+}
+
+//Define a function `viewCart` which does not accept any arguments. This function should
+//loop over every item in `cart` to print out `"In your cart you have [item and price pairs]."`.
+//If there isn't anything in your cart, the function should print out `"Your shopping cart is empty."`.
+
+function viewCart() {
+    if (cart.length < 1) {
+     console.log('Your shopping cart is empty.');
+  }
+  else {
+    var itemAndPrice = []
+ cart.forEach(function (item, price) {
+  itemAndPrice.push(` ${item} at ${price}`)
+});
+
+}
+console.log(`In your cart, you have${itemAndPrice.join(',')}`)
 }
